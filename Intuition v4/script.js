4f5fba3d5867238895013f14352b89ee50aaf652
Tamatoa <timirmusic@gmail.ru>
2022-11-07 19:47:09 +0300

    feat: new version of Intuition

--- Intuition v4/script.js
+++ Intuition v4/script.js
@@ -1,16 +1,17 @@
 ﻿let rnd_value;
 let int_rnd_value;
-let third_sector;
-let li_3;
+let maxLevel = 8;
 let i = 0;
 let stat = 0;
 let level = 1;
 let ul = document.getElementById("wheel__sectors");
-let first_sector = document.querySelector('.first_sector');
-let second_sector = document.querySelector('.second_sector');
+console.log(ul);
+let sectors = document.querySelectorAll('.sectors');
+console.log(sectors);
 let statistics = document.querySelector('.text');
-first_sector.addEventListener("click", function() {pickFunction(0);});
-second_sector.addEventListener("click", function() {pickFunction(1);});
+for (let j = 0; j < maxLevel; j++) {
+    sectors[j].addEventListener("click", function() {pickFunction(j);});
+}
 function randomChoice() {
     rnd_value = (level + 1) * Math.random();
     console.log(rnd_value);
@@ -18,25 +19,11 @@ function randomChoice() {
     console.log(int_rnd_value);
 }
 function showGreenSector() {
-    if (int_rnd_value == 0) {
-        first_sector.style.backgroundColor = "green";
-        setTimeout(function(){
-            first_sector.removeAttribute("style");
-        }, 400);
-    }
-    else if (int_rnd_value == 1) {
-        second_sector.style.backgroundColor = "green";
-        setTimeout(function(){
-            second_sector.removeAttribute("style");
-        }, 400); 
-    }
-    else {
-        third_sector.style.backgroundColor = "green";
-        setTimeout(function(){
-            third_sector.removeAttribute("style");
-        }, 400);       
-    }
-}
+    sectors[int_rnd_value].setAttribute("id", "green");
+    setTimeout(function(){
+        sectors[int_rnd_value].removeAttribute("id", "green");
+    }, 380);
+}    
 function pickFunction(sector_number) {
     if (sector_number == int_rnd_value) {
         console.log("correct");
@@ -45,10 +32,10 @@ function pickFunction(sector_number) {
         statistics.innerText = Math.round(stat).toString() + "%";
         i++;
         showGreenSector();
-        if (level == 1) {
+        if (level < maxLevel - 1) {
             setTimeout(function(){
                 addElements();
-                level = 2;
+                level++;
                 randomChoice();
             }, 390);
         }
@@ -61,7 +48,7 @@ function pickFunction(sector_number) {
         statistics.innerText = Math.round(stat).toString() + "%";
         i++;
         showGreenSector();
-        if (level == 2) {
+        if (level > 1) {
             setTimeout(function() {
                 removeElements();
                 level = 1;
@@ -74,19 +61,82 @@ function pickFunction(sector_number) {
     }, 395);
 }
 function addElements() {
-    li_3 = document.createElement("li");
-    first_sector.setAttribute("id", "sec_1");
-    second_sector.setAttribute("id", "sec_2");
-    li_3.setAttribute("id", "sec_3");
-    li_3.setAttribute("class", "third_sector");
-    ul.appendChild(first_sector);
-    ul.appendChild(second_sector);
-    ul.appendChild(li_3);
-    third_sector = document.querySelector('.third_sector');
-    third_sector.addEventListener("click", function() {pickFunction(2);});
-  }
+    let step = 360 / (level + 2);
+    // if (level == 1) {
+    //     sectors[0].style.transform = "rotate(-30deg) skew(-30deg) scale(2)";
+    //     sectors[1].style.transform = "rotate(90deg) skew(-30deg) scale(2)";
+    //     sectors[2].style.display = "block";
+    //     sectors[2].style.transform = "rotate(210deg) skew(-30deg) scale(2)";
+    // }
+    
+        for (let j = 0; j < level + 2; j++) {
+            sectors[j].style.display = "block";
+            sectors[j].style.transform = "rotate(" + (90 - step / 2 + j * step) + "deg) skew(" + (90 - step) + "deg) scale(2)";
+        }
+    
+    // else if (level == 2) {
+    //     sectors[0].style.transform = "rotate(45deg) skew(0deg) scale(2)";
+    //     sectors[1].style.transform = "rotate(135deg) skew(0deg) scale(2)";
+    //     sectors[2].style.display = "block";
+    //     sectors[2].style.transform = "rotate(225deg) skew(0deg) scale(2)";
+    //     sectors[3].style.display = "block";
+    //     sectors[3].style.transform = "rotate(315deg) skew(0deg) scale(2)";
+    // }
+    // else if (level == 3) {
+    //     sectors[0].style.transform = "rotate(54deg) skew(18deg) scale(2)";
+    //     sectors[1].style.transform = "rotate(126deg) skew(18deg) scale(2)";
+    //     sectors[2].style.display = "block";
+    //     sectors[2].style.transform = "rotate(198deg) skew(18deg) scale(2)";
+    //     sectors[3].style.display = "block";
+    //     sectors[3].style.transform = "rotate(270deg) skew(18deg) scale(2)";
+    //     sectors[4].style.display = "block";
+    //     sectors[4].style.transform = "rotate(342deg) skew(18deg) scale(2)";
+    // }
+    // else if (level == 4) {
+    //     sectors[0].style.transform = "rotate(60deg) skew(30deg) scale(2)";
+    //     sectors[1].style.transform = "rotate(120deg) skew(30deg) scale(2)";
+    //     sectors[2].style.display = "block";
+    //     sectors[2].style.transform = "rotate(180deg) skew(30deg) scale(2)";
+    //     sectors[3].style.display = "block";
+    //     sectors[3].style.transform = "rotate(240deg) skew(30deg) scale(2)";
+    //     sectors[4].style.display = "block";
+    //     sectors[4].style.transform = "rotate(300deg) skew(30deg) scale(2)";
+    //     sectors[5].style.display = "block";
+    //     sectors[5].style.transform = "rotate(360deg) skew(30deg) scale(2)";
+    // }
+    // else if (level == 5) {
+    //     sectors[0].style.transform = "rotate(64.286deg) skew(38.571deg) scale(2)";
+    //     sectors[1].style.transform = "rotate(115.714deg) skew(38.571deg) scale(2)";
+    //     sectors[2].style.display = "block";
+    //     sectors[2].style.transform = "rotate(167.143deg) skew(38.571deg) scale(2)";
+    //     sectors[3].style.display = "block";
+    //     sectors[3].style.transform = "rotate(218.571deg) skew(38.571deg) scale(2)";
+    //     sectors[4].style.display = "block";
+    //     sectors[4].style.transform = "rotate(270deg) skew(38.571deg) scale(2)";
+    //     sectors[5].style.display = "block";
+    //     sectors[5].style.transform = "rotate(321.429deg) skew(38.571deg) scale(2)";
+    //     sectors[6].style.display = "block";
+    //     sectors[6].style.transform = "rotate(372.857deg) skew(38.571deg) scale(2)";
+    // }
+    // else if (level == 6) {
+    //     sectors[0].style.transform = "rotate(67.5deg) skew(45deg) scale(2)";
+    //     sectors[1].style.transform = "rotate(112.5deg) skew(45deg) scale(2)";
+    //     sectors[2].style.display = "block";
+    //     sectors[2].style.transform = "rotate(157.5deg) skew(45deg) scale(2)";
+    //     sectors[3].style.display = "block";
+    //     sectors[3].style.transform = "rotate(202.5deg) skew(45deg) scale(2)";
+    //     sectors[4].style.display = "block";
+    //     sectors[4].style.transform = "rotate(247.5deg) skew(45deg) scale(2)";
+    //     sectors[5].style.display = "block";
+    //     sectors[5].style.transform = "rotate(292.5deg) skew(45deg) scale(2)";
+    //     sectors[6].style.display = "block";
+    //     sectors[6].style.transform = "rotate(337.5deg) skew(45deg) scale(2)";
+    //     sectors[7].style.display = "block";
+    //     sectors[7].style.transform = "rotate(382.5deg) skew(45deg) scale(2)";
+    // }
+}
 function removeElements() {
-    li_3.remove();
-    first_sector.removeAttribute("id");
-    second_sector.removeAttribute("id");
-  }
+    for (let j = 0; j < maxLevel; j++) {
+        sectors[j].removeAttribute("style");
+    }
+}
